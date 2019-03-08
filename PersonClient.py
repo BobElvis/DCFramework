955adff2553f2bf4bad09c1e16a5a9e4c86376a1
Trym Tarjeison Lekva <trymtl@dhcp-10-24-202-46.wlan.ntnu.no>
2019-03-08 20:51:52 +0100

    implemented all calls which needs token

--- PersonClient.py
+++ PersonClient.py
@@ -45,5 +45,84 @@ class Person:
         )
         return requests.get(request_url, headers=headers)
 
+    def put(self, path, params):
+        request_url, headers = self.url_header_generator.generate(
+            path=path, params=params, api_token=self.api_token, api_key=api_key, method="PUT"
+        )
+        return requests.put(request_url, headers=headers)
+
+    def post(self, path, params, data):
+        request_url, headers = self.url_header_generator.generate(
+            path=path, params=params, api_token=self.api_token, api_key=api_key, method="POST"
+        )
+        return requests.post(request_url, data=data, headers=headers)
+
+    def delete(self, path, params):
+        request_url, headers = self.url_header_generator.generate(
+            path=path, params=params, api_token=self.api_token, api_key=api_key, method="POST"
+        )
+        return requests.delete(request_url, headers=headers)
+
     def get_accounts(self):
         return self.get("/accounts/", {})
+
+    def get_account_details(self, account_number: str):
+        return self.get("/accounts/" + account_number, {})
+
+    def get_account_balance(self, account_number: str):
+        return self.get("/accounts/" + account_number + "/balance", {})
+
+    def get_cards(self):
+        return self.get("/cards/", {})
+
+    def get_card_details(self, card_id: str):
+        return self.get("/cards/" + card_id, {})
+
+    def get_card_balance(self, card_id: str):
+        return self.get("/cards/" + card_id + "/balance", {})
+
+    def get_customer_details(self):
+        return self.get("/customers/current", {})
+
+    def get_due_payments(self, account_number: str):
+        return self.get("/payments/" + account_number + "/due", {})
+
+    def get_due_payments_by_id(self, account_number: str, payment_id: str):
+        return self.get("/payments/" + account_number + "/due/" + payment_id, {})
+
+    def get_transactions(self, account_number: str):
+        return self.get("/transactions/" + account_number + "/", {})
+
+    def put_block_card(self, card_id):
+        return self.put("/cards/" + card_id + "/block", {})
+
+    def put_unblock_card(self, card_id):
+        return self.put("/cards/" + card_id + "/unblock", {})
+
+    # NOT WORKING? However APIs are static sooooooo...
+    def post_initiate_payment(self, body):
+        return self.post("/payments/", {}, body)
+
+    # NOT TESTED
+    def delete_payment(self, account_number: str, payment_id: str):
+        return self.delete("/payments/" + account_number + "/pending-payments/" + payment_id, {})
+
+
+class PaymentDto:
+    def __init__(self, kid: [str, None],
+                 debit_account_number: str,
+                 credit_account_number: str,
+                 amount: float,
+                 requested_execution_date: str,
+                 country: [str, None],
+                 currency: [str, None],
+                 immediate_payment: [bool, None]):
+
+        self.kid = kid
+        self.debitAccountNumber = debit_account_number
+        self.creditAccountNumber = credit_account_number
+        self.amount = amount,
+        self.requestedExecutionDate = requested_execution_date
+        self.country = country
+        self.currency = currency
+        self.immediatePayment = immediate_payment
--- dnb.py
+++ dnb.py
@@ -1,11 +1,66 @@
 import json
-from PersonClient import Person
+from PersonClient import Person, PaymentDto
+
+
+def print_response(response):
+    print(json.dumps(response.json(), indent=4, sort_keys=False))
 
 
 def main():
+    # Make person object with SSN
     p = Person(29105573083)
-    print(json.dumps(p.get_accounts().json(), indent=4, sort_keys=False))
 
+    account_print = False
+    card_print = True
+    customer_print = True
+    payment_print = True
+
+
+    # ACCOUNTS:
+    if account_print:
+        accounts = p.get_accounts()
+        print_response(accounts)
+
+        account_number = accounts.json()['accounts'][0]['accountNumber']
+
+        account_details = p.get_account_details(account_number)
+        print_response(account_details)
+
+        account_balance = p.get_account_balance(account_number)
+        print_response(account_balance)
+
+    # CARDS:
+    if card_print:
+        cards = p.get_cards()
+        print_response(cards)
+
+        card_id = cards.json()[1]['cardId']
+
+        print_response(p.get_card_details(card_id))
+
+        # PS: Only works for credit cards. Use get_account_balance for debit cards.
+        print_response(p.get_card_balance(card_id))
+
+        # Card in pos 0 has flag unblockAllowed=true
+        print_response(p.put_unblock_card(cards.json()[0]['cardId']))
+
+    # CUSTOMERS
+    if customer_print:
+        print_response(p.get_customer_details())
+
+    # PAYMENT - Static data still so why did I even do this?
+    if payment_print:
+        accounts = p.get_accounts()
+
+        debit = accounts.json()['accounts'][0]['accountNumber']
+        credit = accounts.json()['accounts'][1]['accountNumber']
+
+        payDto = PaymentDto(None, debit, credit, 200, "2019-03-09", None, None, None)
+
+        # Getting 403 here, maybe I am not so good.
+        print(p.post_initiate_payment(json.dumps(payDto.__dict__)))
+
+        print_response(p.get_transactions(debit))
 
 if __name__ == "__main__":
     main()
